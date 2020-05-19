df643fc9a666d19e10ec2f5a0050b595f4d0f071
naglec13 <naglec13@gmail.com>
2020-05-19 23:39:04 +0300

    ..fill in details about the task

--- src/main/java/com/coronagoaway/CoronaDesinfector.java
+++ src/main/java/com/coronagoaway/CoronaDesinfector.java
@@ -1,4 +1,15 @@
 package com.coronagoaway;
 
 public class CoronaDesinfector {
+
+    public void Start(Room room){
+//        todo сообщить всем присутствующим в комнате, что о начале дезинфекции, и попросить всех свалить
+//        todo разогнать всех кто не вышел после объявления
+        desinfect(room);
+//        todo сообщить всем присутствующим в комнате, что они могут вернуться обратно
+    }
+
+    private void desinfect(Room room) {
+        System.out.println("зачитывается молитва: 'корона изыди!' - молитва прочитана, вирус низвергнут в ад");
+    }
 }
