10e4f20c777d1ffe8c41ef4da6a2780e2f19a780
naglec13 <naglec13@gmail.com>
2020-05-21 23:54:48 +0300

    ..add one more impl for Policeman interface

---
+++ src/main/java/com/coronagoaway/AngryPoliceman.java
@@ -0,0 +1,8 @@
+package com.coronagoaway;
+
+public class AngryPoliceman implements Policeman {
+    @Override
+    public void makePeopleLeaveRoom() {
+        System.out.println("Все вон, буду стрелять..где мой пистолет, вы у меня получите!");
+    }
+}
--- src/main/java/com/coronagoaway/JavaConfig.java
+++ src/main/java/com/coronagoaway/JavaConfig.java
@@ -16,7 +16,7 @@ public class JavaConfig implements Config {
     public <T> Class<? extends T> getInstance(Class<T> ifc) {
         Set<Class<? extends T>> classes = scanner.getSubTypesOf(ifc);
             if (classes.size() != 1) {
-                throw new RuntimeException(ifc+ ": there is 0 or more than one implementations");
+                throw new RuntimeException(ifc+ ": has 0 or more than one implementation");
             }
         return classes.iterator().next();
     }
