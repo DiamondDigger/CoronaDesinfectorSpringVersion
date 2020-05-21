d5031df15b1146abd1e79db9219d36e3bab96491
naglec13 <naglec13@gmail.com>
2020-05-22 00:04:53 +0300

    ..create AlarmPoliceman impl of Policeman

---
+++ src/main/java/com/coronagoaway/AlarmPoliceman.java
@@ -0,0 +1,8 @@
+package com.coronagoaway;
+
+public class AlarmPoliceman implements Policeman {
+    @Override
+    public void makePeopleLeaveRoom() {
+        System.out.println("ALARM!!ALARM!!..3...2...1..");
+    }
+}
