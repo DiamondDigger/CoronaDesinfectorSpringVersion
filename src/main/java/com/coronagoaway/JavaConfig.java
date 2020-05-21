958327afb25e096f4f8cf9a771eef8655a02c709
naglec13 <naglec13@gmail.com>
2020-05-21 23:57:08 +0300

    ..change condition in JavaConfig.class and ObjectFactory create AngryPoliceman impl of Policeman interface..?

--- src/main/java/com/coronagoaway/JavaConfig.java
+++ src/main/java/com/coronagoaway/JavaConfig.java
@@ -15,7 +15,7 @@ public class JavaConfig implements Config {
     @Override
     public <T> Class<? extends T> getInstance(Class<T> ifc) {
         Set<Class<? extends T>> classes = scanner.getSubTypesOf(ifc);
-            if (classes.size() != 1) {
+            if (classes.size() >3) {
                 throw new RuntimeException(ifc+ ": has 0 or more than one implementation");
             }
         return classes.iterator().next();
