c8c86599c746a07706edbd7230dddd103abe9275
naglec13 <naglec13@gmail.com>
2020-05-29 23:15:02 +0300

    ..try to rebuild logic with new class Application and method run() in it

--- src/main/java/com/coronagoaway/Application.java
+++ src/main/java/com/coronagoaway/Application.java
@@ -1,6 +1,12 @@
 package com.coronagoaway;
 
+import java.util.Map;
+import java.util.Set;
+
 public class Application {
-    public void run(){
+    public void run(String packageScan, Map<Class, Class> ifc2ImplClass){
+        JavaConfig config = new JavaConfig(packageScan,ifc2ImplClass);
+        Set<Class<? extends ObjectConfigurator>> subTypesOf = config.getScanner().getSubTypesOf(ObjectConfigurator.class);
+
     }
 }
