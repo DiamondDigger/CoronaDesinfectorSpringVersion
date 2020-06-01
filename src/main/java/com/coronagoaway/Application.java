f1b1603334eb81f95f8b2daaf3d3e30f6e1c7a27
naglec13 <naglec13@gmail.com>
2020-06-01 11:56:31 +0300

    ..make Application.run() static

--- src/main/java/com/coronagoaway/Application.java
+++ src/main/java/com/coronagoaway/Application.java
@@ -3,7 +3,7 @@ package com.coronagoaway;
 import java.util.Map;
 
 public class Application {
-    public ApplicationContext run(String packageScan, Map<Class, Class> ifc2ImplClass){
+    public static ApplicationContext run(String packageScan, Map<Class, Class> ifc2ImplClass){
         JavaConfig config = new JavaConfig(packageScan,ifc2ImplClass);
         ApplicationContext context = new ApplicationContext(config);
         ObjectFactory objectFactory = new ObjectFactory(context);
--- src/main/java/com/coronagoaway/Main.java
+++ src/main/java/com/coronagoaway/Main.java
@@ -5,8 +5,7 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args){
-        Application application = new Application();
-        ApplicationContext context = application.run("com.coronagoaway", new HashMap<>(Map.of(Policeman.class, AngryPoliceman.class)));
+        ApplicationContext context = Application.run("com.coronagoaway", new HashMap<>(Map.of(Policeman.class, AngryPoliceman.class)));
         CoronaDesinfector coronaDesinfector = context.getObject(CoronaDesinfector.class);
         coronaDesinfector.start(new Room());
     }
