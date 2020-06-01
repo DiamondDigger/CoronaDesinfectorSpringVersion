b76f672fc763e7ec84e590c203d261672c54e57b
naglec13 <naglec13@gmail.com>
2020-06-01 12:57:27 +0300

    ..add sout(message) in RecommemndatorImpl constructor in case to check @Singleton

--- src/main/java/com/coronagoaway/AngryPoliceman.java
+++ src/main/java/com/coronagoaway/AngryPoliceman.java
@@ -1,8 +1,11 @@
 package com.coronagoaway;
 
 public class AngryPoliceman implements Policeman {
+    @InjectByType
+    private Recommendator recommendator;
     @Override
     public void makePeopleLeaveRoom() {
+        System.out.println(recommendator.getClass());;
         System.out.println("Все вон, буду стрелять..где мой пистолет, вы у меня получите!");
     }
 }
--- src/main/java/com/coronagoaway/RecommendatorImpl.java
+++ src/main/java/com/coronagoaway/RecommendatorImpl.java
@@ -1,9 +1,13 @@
 package com.coronagoaway;
 
-@Singleton
-public class RecommemndatorImpl implements Recommendator {
+//@Singleton
+public class RecommendatorImpl implements Recommendator {
     @InjectProperty("tea")
     private String alcohol;
+
+    public RecommendatorImpl() {
+        System.out.println("recommendator was created");
+    }
 
     @Override
     public void recommend() {
