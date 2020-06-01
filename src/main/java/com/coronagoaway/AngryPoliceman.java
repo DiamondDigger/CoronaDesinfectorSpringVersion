8ce687706c9116cfcfffe51917fc56132315ce02
naglec13 <naglec13@gmail.com>
2020-06-01 16:58:52 +0300

    ..refactor code, extract some methods in ObjectFactory for better readability

--- src/main/java/com/coronagoaway/AngryPoliceman.java
+++ src/main/java/com/coronagoaway/AngryPoliceman.java
@@ -1,11 +1,19 @@
 package com.coronagoaway;
 
+import javax.annotation.PostConstruct;
+
 public class AngryPoliceman implements Policeman {
     @InjectByType
     private Recommendator recommendator;
+
+    @PostConstruct
+    public void Init(){
+        System.out.println(recommendator.getClass());
+        System.out.println("Что происходит? Я init() метод, где все?");
+    }
+
     @Override
     public void makePeopleLeaveRoom() {
-        System.out.println(recommendator.getClass());;
         System.out.println("Все вон, буду стрелять..где мой пистолет, вы у меня получите!");
     }
 }
--- src/main/java/com/coronagoaway/ObjectFactory.java
+++ src/main/java/com/coronagoaway/ObjectFactory.java
@@ -2,19 +2,21 @@ package com.coronagoaway;
 
 import lombok.SneakyThrows;
 
+import javax.annotation.PostConstruct;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ObjectFactory {
 
-    private ApplicationContext context;
+    private final ApplicationContext context;
     private Config config;
     private List<ObjectConfigurator> configurators = new ArrayList<>();
 
     @SneakyThrows
     public ObjectFactory(ApplicationContext context) {
         this.context = context;
-//        config = new JavaConfig("com.coronagoaway", new HashMap<>(Map.of(Policeman.class, AngryPoliceman.class)));
         for (Class<? extends ObjectConfigurator> aClass : context.getConfig().getScanner().getSubTypesOf(ObjectConfigurator.class)) {
             configurators.add(aClass.getDeclaredConstructor().newInstance());
         }
@@ -24,11 +26,31 @@ public class ObjectFactory {
     @SneakyThrows
     public <T> T createObject(Class<T> impClass) {
 
-        T t = impClass.getDeclaredConstructor().newInstance();
+        T t = create(impClass);
+
+        confiqure(t);
+
+        invokeInit(impClass, t);
+
+        return t;
+    }
+
+    private <T> void confiqure(T t) {
         for (ObjectConfigurator configurator : configurators) {
             configurator.configure(t, context);
         }
-        return t;
+    }
+
+    private <T> T create(Class<T> impClass) throws InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+        return impClass.getDeclaredConstructor().newInstance();
+    }
+
+    private <T> void invokeInit(Class<T> impClass, T t) throws IllegalAccessException, InvocationTargetException {
+        for (Method method : impClass.getMethods()) {
+            if (method.isAnnotationPresent(PostConstruct.class)) {
+                method.invoke(t);
+            }
+        }
     }
 }
 
